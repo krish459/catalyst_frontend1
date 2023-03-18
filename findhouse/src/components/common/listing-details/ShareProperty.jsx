3d5230402153b2f3eea3c354e4add3182cd10db9
Krish <krishshah235@gmail.com>
2023-03-18 15:21:27 +0530

    Reference error resolved

--- findhouse/src/components/common/listing-details/ShareProperty.jsx
+++ findhouse/src/components/common/listing-details/ShareProperty.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { FaCopy } from "react-icons/fa";
 
 const ShareProperty = () => {
   const [isCopied, setIsCopied] = useState(false);
+  const [propertyUrl, setPropertyUrl] = useState('');
+
+  useEffect(() => {
+    setPropertyUrl(window.location.href);
+  }, []);
 
   const handleCopy = () => {
     setIsCopied(true);
@@ -16,7 +21,7 @@ const ShareProperty = () => {
       <div className="share-property__actions">
         <div>
 
-        <CopyToClipboard text={window.location.href} onCopy={handleCopy}>
+        <CopyToClipboard text={propertyUrl} onCopy={handleCopy}>
           <button className="btn btn1 float-start share-property__button" aria-label="Copy link">
             <FaCopy className="share-property__icon" />
             {isCopied ? "Copied!" : "Copy Link"}
@@ -24,7 +29,7 @@ const ShareProperty = () => {
         </CopyToClipboard>
         </div>
         <div className="share-property__more">
-          {/* <a className="share-property__link" href={window.location.href}>
+          {/* <a className="share-property__link" href={propertyUrl}>
             More Details Here
           </a> */}
         </div>
@@ -36,28 +41,39 @@ const ShareProperty = () => {
 export default ShareProperty;
 
 
-// import React from "react";
+
+// import React, { useState } from "react";
+// import { CopyToClipboard } from "react-copy-to-clipboard";
+// import { FaCopy } from "react-icons/fa";
 
 // const ShareProperty = () => {
-//   const copyLinkToClipboard = () => {
-//     const link = window.location.href;
-//     navigator.clipboard.writeText(link);
-//     alert("Link copied to clipboard!");
+//   const [isCopied, setIsCopied] = useState(false);
+
+//   const handleCopy = () => {
+//     setIsCopied(true);
+//     setTimeout(() => setIsCopied(false), 3000);
 //   };
 
 //   return (
-//     <>
-//     <div style={{height:"5rem"}}>
+//     <div className="share-property" style={{height:"5rem"}}>
+//       <h4 className="share-property__title">Share Property Link</h4>
+//       <div className="share-property__actions">
+//         <div>
 
-//       <h4 className="mb30">Share Property Link</h4>
-//       <button className="btn btn1 float-start" onClick={copyLinkToClipboard}>Copy Link</button>
-//       <div style={{marginTop:"0.5rem"}}>
-//       {/* <a className="more_info" href={window.location.href}>
-//         More Details Here
-//     </a> */}
+//         <CopyToClipboard text={window.location.href} onCopy={handleCopy}>
+//           <button className="btn btn1 float-start share-property__button" aria-label="Copy link">
+//             <FaCopy className="share-property__icon" />
+//             {isCopied ? "Copied!" : "Copy Link"}
+//           </button>
+//         </CopyToClipboard>
+//         </div>
+//         <div className="share-property__more">
+//           {/* <a className="share-property__link" href={window.location.href}>
+//             More Details Here
+//           </a> */}
+//         </div>
 //       </div>
 //     </div>
-//     </>
 //   );
 // };
 
